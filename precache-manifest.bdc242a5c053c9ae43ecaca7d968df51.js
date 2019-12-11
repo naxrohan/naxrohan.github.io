f981c6ba0132c82371456234de27e56a98b7fbf7
naxrohan <rohan.jd@hotmail.com>
2019-12-11 15:42:17 +0530

    First build added --manually

---
+++ precache-manifest.bdc242a5c053c9ae43ecaca7d968df51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f45e6e2232e08421c420f74b67fcb48",
+    "url": "/my-html-site/index.html"
+  },
+  {
+    "revision": "655a4d900dd4de89c06e",
+    "url": "/my-html-site/static/css/main.91e4f448.chunk.css"
+  },
+  {
+    "revision": "91442cc3bf2d618b5a63",
+    "url": "/my-html-site/static/js/2.38f9c169.chunk.js"
+  },
+  {
+    "revision": "655a4d900dd4de89c06e",
+    "url": "/my-html-site/static/js/main.22b9f8e6.chunk.js"
+  },
+  {
+    "revision": "e0b6f43d5317ff7097e9",
+    "url": "/my-html-site/static/js/runtime~main.64590c0e.js"
+  }
+]);
